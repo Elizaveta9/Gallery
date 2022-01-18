b59de750c1094dbacf96ab8cdf66da10e2430e97
Elizaveta9 <katkova_lizochka@bk.ru>
2022-01-18 18:07:04 +0300

    Переименован класс cell в Cell

--- app/src/main/java/com/example/gallery/Cell.java
+++ app/src/main/java/com/example/gallery/Cell.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class cell extends AppCompatActivity {
+public class Cell extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
